60f21f51968087076f30efacc401a07c23e80ce3
Tyrone Valmores <aichevalmores@gmail.com>
2018-06-23 21:30:58 -0700

    test

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ function showMobileNav()
     {
         mNav.style.transform = "scale(1)";
         ctr = 1;
-        console.log("hi!");
     }
     else if(ctr == 1)
     {
@@ -46,51 +45,17 @@ function next()
 {
     if(counter == 1)
     {
-        contentOne.style.opacity = "0";
-        contentTwo.style.opacity = "1";
-        contentThree.style.opacity = "0";
+        activeOne();
         counter = 2;
-        btnTwo.classList.remove("active");
-        btnTwo.classList.add("deactive");
-
-        btn.classList.remove("deactive");
-        btn.classList.add("active");
-
-        btnThree.classList.remove("active");
-        btnThree.classList.add("deactive");
     }
     else if(counter == 2)
     {
-        contentOne.style.opacity = "0";
-        contentThree.style.opacity = "1";
-        contentTwo.style.opacity = "0";
-
-        btnTwo.classList.remove("active");
-        btnTwo.classList.add("deactive");
-
-        btn.classList.remove("active");
-        btn.classList.add("deactive");
-
-        btnThree.classList.remove("deactive");
-        btnThree.classList.add("active");
-
+        activeThree();
         counter = 3;
     }
     else if(counter == 3)
     {
-        contentThree.style.opacity = "0";
-        contentTwo.style.opacity = "0";
-        contentOne.style.opacity = "1";
-
-        btnTwo.classList.remove("deactive");
-        btnTwo.classList.add("active");
-
-        btnThree.classList.remove("active");
-        btnThree.classList.add("deactive");
-
-        btn.classList.remove("active");
-        btn.classList.add("deactive");
-
+        activeTwo();
         counter = 1;
     }
 }
@@ -99,53 +64,18 @@ function previous()
 {
     if(counter == 2)
     {
-        contentThree.style.opacity = "0";
-        contentTwo.style.opacity = "0";
-        contentOne.style.opacity = "1";
-        
-        btnTwo.classList.remove("deactive");
-        btnTwo.classList.add("active");
-
-        btnThree.classList.remove("active");
-        btnThree.classList.add("deactive");
-
-        btn.classList.remove("active");
-        btn.classList.add("deactive");
-
+        activeTwo();
         counter = 1;
     }
     else if(counter == 1)
     {
-        contentOne.style.opacity = "0";
-        contentThree.style.opacity = "1";
-        contentTwo.style.opacity = "0";
-
-        btnTwo.classList.remove("active");
-        btnTwo.classList.add("deactive");
-
-        btn.classList.remove("active");
-        btn.classList.add("deactive");
-
-        btnThree.classList.remove("deactive");
-        btnThree.classList.add("active");
-
+        activeThree();
         counter = 3;
     }
     else if(counter == 3)
     {
-        contentOne.style.opacity = "0";
-        contentTwo.style.opacity = "1";
-        contentThree.style.opacity = "0";
-
+        activeOne();
         counter = 2;
-        btnTwo.classList.remove("active");
-        btnTwo.classList.add("deactive");
-
-        btn.classList.remove("deactive");
-        btn.classList.add("active");
-
-        btnThree.classList.remove("active");
-        btnThree.classList.add("deactive");
     }
 }
 
@@ -154,13 +84,12 @@ function activeOne()
 {
     contentOne.style.opacity = "0";
     contentTwo.style.opacity = "1";
+    contentThree.style.opacity = "0";
     counter = 2;
     btnTwo.classList.remove("active");
     btnTwo.classList.add("deactive");
-
     btn.classList.remove("deactive");
     btn.classList.add("active");
-
     btnThree.classList.remove("active");
     btnThree.classList.add("deactive");
 }
@@ -172,16 +101,12 @@ function activeTwo()
     contentOne.style.opacity = "1";
     contentTwo.style.opacity = "0";
     contentThree.style.opacity = "0";
-
     btnTwo.classList.remove("deactive");
     btnTwo.classList.add("active");
-
     btn.classList.remove("active");
     btn.classList.add("deactive");
-
     btnThree.classList.remove("active");
     btnThree.classList.add("deactive");
-
     counter = 3;
 }
 
@@ -191,16 +116,11 @@ function activeThree()
     contentOne.style.opacity = "0";
     contentThree.style.opacity = "1";
     contentTwo.style.opacity = "0";
-
     btnTwo.classList.remove("active");
     btnTwo.classList.add("deactive");
-
     btnThree.classList.remove("deactive");
     btnThree.classList.add("active");
-
     btn.classList.remove("active");
     btn.classList.add("deactive");
-
     counter = 1;
-    console.log("im no 3");
 }
